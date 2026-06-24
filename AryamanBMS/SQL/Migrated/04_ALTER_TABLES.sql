af9a8c38b8a2f61dab587d22d74a6013ca5c4967
Atharv Warkari <atharv.warkari@rediffmail.com>
2026-06-24 15:56:01 +0530

    Alter

--- AryamanBMS/SQL/Migrated/04_ALTER_TABLES.sql
+++ AryamanBMS/SQL/Migrated/04_ALTER_TABLES.sql
@@ -2,9 +2,22 @@
 -- ADD COLUMN `MiddleName` VARCHAR(100) NULL
 -- AFTER `FirstName`;
 
-ALTER TABLE `TableEmployee`
-ADD COLUMN `EsicNo` VARCHAR(10) NULL
-AFTER `UanNo`;
+-- ALTER TABLE `TableEmployee`
+-- ADD COLUMN `EsicNo` VARCHAR(10) NULL
+-- AFTER `UanNo`;
 
-CREATE INDEX `IX_TableEmployee_EsicNo`
-ON `TableEmployee` (`EsicNo`);
+-- CREATE INDEX `IX_TableEmployee_EsicNo`
+-- ON `TableEmployee` (`EsicNo`);
+
+ALTER TABLE `TableEmployee`
+ADD COLUMN `LocalAddress` TEXT NULL
+    AFTER `PermanentAddress`,
+
+ADD COLUMN `LocalCity` VARCHAR(100) NULL
+    AFTER `LocalAddress`,
+
+ADD COLUMN `LocalState` VARCHAR(100) NULL
+    AFTER `LocalCity`,
+
+ADD COLUMN `LocalPinCode` VARCHAR(6) NULL
+    AFTER `LocalState`;
